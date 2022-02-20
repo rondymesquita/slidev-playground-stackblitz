4e0d66c7aff766435e88388d8e55cff695933960
rondymesquita <rondinellimesquitas@gmail.com>
2022-02-20 10:28:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# slidev-gviniy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/slidev-gviniy)
---
+++ components/Counter.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// const fetch = (url) => {
+//   console.log('>>> called', url)
+// 	return new Promise((resolve, reject) => {
+// 	https.get(url, (resp) => {
+
+// 			let data = '';
+
+// 			resp.on('data', (chunk) => {
+// 				data += chunk;
+// 			});
+// 			resp.on('end', () => {
+// 				console.log(data);
+//         resolve('DONE')
+// 				// resolve(JSON.parse(data))
+// 			});
+
+// 		}).on("error", (err) => {
+// 			reject(err)
+// 		});
+// 	})
+// }
+
+const props = defineProps({
+  path: {
+    default: null,
+  },
+})
+
+const data = ref("")
+
+console.log('>>>', props)
+fetch("/" + props.path)
+.then(result => result.text())
+.then((result) => {
+  let f = result
+  console.log(f)
+  data.value = f
+})
+</script>
+
+<template>
+  <div flex="~" w="min" border="~ gray-400 opacity-50 rounded-md">
+    <pre>
+      {{data}}
+    </pre>
+  </div>
+</template>
